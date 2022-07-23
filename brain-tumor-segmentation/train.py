9a13828f9a0b9e6386465f8c2e0d613df4034986
avocadopelvis <avotheyo23@gmail.com>
2022-07-23 23:56:28 +0530

    add train.py

---
+++ brain-tumor-segmentation/train.py
@@ -0,0 +1,288 @@
+# LOAD LIBRARIES
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import os
+import cv2
+# glob (short for global) is used to return all file paths that match a specific pattern.
+import glob 
+
+# NEURAL IMAGING
+import nilearn as nl
+import nibabel as nib # access a multitude of neuroimaging data formats
+import nilearn.plotting as nlplt
+import gif_your_nifti.core as gif2nif
+
+
+# ML Libraries
+import keras
+import keras.backend as K
+from keras.callbacks import CSVLogger
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import plot_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, TensorBoard
+from tensorflow.keras.layers.experimental import preprocessing
+
+# make numpy printouts easier to read
+np.set_printoptions(precision = 3, suppress = True)
+
+# define segmentation areas
+SEGMENT_CLASSES = {
+    0 : 'NOT TUMOR',
+    1 : 'NECROTIC/CORE', # or NON-ENHANCING TUMOR CORE
+    2 : 'EDEMA',
+    3 : 'ENHANCING' # original 4 -> converted into 3 later
+}
+
+# there are 155 slices per volume
+# to start at 5 and use 145 slices means we will skip the first 5 and last 5 
+VOLUME_SLICES = 100 
+VOLUME_START_AT = 22 # first slice of volume that we will include
+
+# dataset path
+train_data = ""
+valid_data = ""
+
+# dice loss as defined above for 4 classes
+def dice_coef(y_true, y_pred, smooth=1.0):
+    class_num = 4
+    for i in range(class_num):
+        y_true_f = K.flatten(y_true[:,:,:,i])
+        y_pred_f = K.flatten(y_pred[:,:,:,i])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss = ((2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+   #     K.print_tensor(loss, message='loss value for class {} : '.format(SEGMENT_CLASSES[i]))
+        if i == 0:
+            total_loss = loss
+        else:
+            total_loss = total_loss + loss
+    total_loss = total_loss / class_num
+#    K.print_tensor(total_loss, message=' total dice coef: ')
+    return total_loss
+
+
+# define per class evaluation of dice coef
+def dice_coef_necrotic(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,1] * y_pred[:,:,:,1]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,1])) + K.sum(K.square(y_pred[:,:,:,1])) + epsilon)
+
+def dice_coef_edema(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,2] * y_pred[:,:,:,2]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,2])) + K.sum(K.square(y_pred[:,:,:,2])) + epsilon)
+
+def dice_coef_enhancing(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,3] * y_pred[:,:,:,3]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,3])) + K.sum(K.square(y_pred[:,:,:,3])) + epsilon)
+
+
+# Computing Precision 
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    
+# Computing Sensitivity      
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+
+# Computing Specificity
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+
+
+# U-NET
+def build_unet(inputs, ker_init, dropout):
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(inputs)
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv1)
+    
+    pool = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool)
+    conv = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv)
+    
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv2)
+    
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv3)
+    
+    
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool4)
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv5)
+    drop5 = Dropout(dropout)(conv5)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(drop5))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv9)
+    
+    up = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(conv9))
+    merge = concatenate([conv1,up], axis = 3)
+    conv = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge)
+    conv = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv)
+    
+    conv10 = Conv2D(4, (1,1), activation = 'softmax')(conv)
+    
+    return Model(inputs = inputs, outputs = conv10)
+
+
+IMG_SIZE = 128
+input_layer = Input((IMG_SIZE, IMG_SIZE, 2))
+
+model = build_unet(input_layer, 'he_normal', 0.2)
+model.compile(
+    loss = "categorical_crossentropy",
+    optimizer = keras.optimizers.Adam(learning_rate = 0.001),
+    metrics = ['accuracy', tf.keras.metrics.MeanIoU(num_classes = 4), dice_coef, precision, sensitivity, specificity, dice_coef_necrotic, dice_coef_edema, dice_coef_enhancing]
+)
+
+# LOAD DATA
+
+# list of directories
+train_val_directories = [f.path for f in os.scandir(train_data) if f.is_dir()]
+
+# remove BraTS20_Training_355 since it has ill formatted name for seg.nii file
+train_val_directories.remove(train_data + 'BraTS20_Training_355')
+
+# function to convert list of paths into IDs
+def pathListIntoIDs(dirList):
+  x = []
+  for i in range(0, len(dirList)):
+    x.append(dirList[i][dirList[i].rfind('/')+1:])
+  return x
+
+ids = pathListIntoIDs(train_val_directories)
+
+# split ids into train+test and validation
+train_test_ids, val_ids = train_test_split(ids, test_size = 0.2)
+# split train+test into train and test                                           
+train_ids, test_ids = train_test_split(train_test_ids, test_size = 0.15)
+
+# override keras sequence DataGenerator class
+class DataGenerator(keras.utils.Sequence):
+    # generates data for Keras
+    def __init__(self, list_IDs, dim=(IMG_SIZE,IMG_SIZE), batch_size = 1, n_channels = 2, shuffle=True):
+        # Initialization
+        self.dim = dim
+        self.batch_size = batch_size
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        # denotes the number of batches per epoch
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        # generate one batch of data
+
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        Batch_ids = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(Batch_ids)
+
+        return X, y
+
+    def on_epoch_end(self):
+        # updates indexes after each epoch
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, Batch_ids):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # initialization
+        X = np.zeros((self.batch_size*VOLUME_SLICES, *self.dim, self.n_channels))
+        y = np.zeros((self.batch_size*VOLUME_SLICES, 240, 240))
+        Y = np.zeros((self.batch_size*VOLUME_SLICES, *self.dim, 4))
+
+        
+        # Generate data
+        for c, i in enumerate(Batch_ids):
+            case_path = os.path.join(train_data, i)
+
+            data_path = os.path.join(case_path, f'{i}_flair.nii');
+            flair = nib.load(data_path).get_fdata()    
+
+            data_path = os.path.join(case_path, f'{i}_t1ce.nii');
+            ce = nib.load(data_path).get_fdata()
+            
+            data_path = os.path.join(case_path, f'{i}_seg.nii');
+            seg = nib.load(data_path).get_fdata()
+        
+            for j in range(VOLUME_SLICES):
+                 X[j +VOLUME_SLICES*c,:,:,0] = cv2.resize(flair[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE));
+                 X[j +VOLUME_SLICES*c,:,:,1] = cv2.resize(ce[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE));
+
+                 y[j +VOLUME_SLICES*c] = seg[:,:,j+VOLUME_START_AT];
+                    
+        # Generate masks
+        y[y==4] = 3;
+        mask = tf.one_hot(y, 4);
+        Y = tf.image.resize(mask, (IMG_SIZE, IMG_SIZE));
+        return X/np.max(X), Y
+
+training_generator = DataGenerator(train_ids)
+valid_generator = DataGenerator(val_ids)
+test_generator = DataGenerator(test_ids)
+
+# add callback for training process
+csv_logger = CSVLogger('training.log', separator=',', append=False)
+
+
+callbacks = [
+#     keras.callbacks.EarlyStopping(monitor='loss', min_delta=0,
+#                               patience=2, verbose=1, mode='auto'),
+      keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                              patience=2, min_lr=0.000001, verbose=1),
+#  keras.callbacks.ModelCheckpoint(filepath = 'model_.{epoch:02d}-{val_loss:.6f}.m5',
+#                             verbose=1, save_best_only=True, save_weights_only = True)
+        csv_logger
+    ]
+
+# TRAIN MODEL
+
+K.clear_session()
+
+history =  model.fit(training_generator,
+                    epochs=35,
+                    steps_per_epoch=len(train_ids),
+                    callbacks= callbacks,
+                    validation_data = valid_generator
+                    )  
+
+# save the model
+model.save("model.h5")
+
